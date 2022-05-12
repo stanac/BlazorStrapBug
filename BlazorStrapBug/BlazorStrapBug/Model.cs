60c46a3412d82e0f81f42f967972ed0425ac8d52
stanac <stanacevm@gmail.com>
2022-05-12 17:48:49 +0200

    bug...

---
+++ BlazorStrapBug/BlazorStrapBug/Model.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorStrapBug;
+
+class Model
+{
+    public Model(string name)
+    {
+        Name = name;
+    }
+
+    public bool IsSelected { get; set; }
+    public string Name { get; set; } = "";
+}
